ab7e2f1baca00beeb5e47254ab11c6e7c681399b
Patrycja Wazner <patrycjawazner21@gmail.com>
2023-08-31 16:29:06 +0100

    Adding Dio into project

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
+    //http://api.weatherapi.com/v1/current.json?key=f8cef05d076a409f853153615233008&q=Żary&aqi=no
+    final response = await Dio().get(
+        'http://api.weatherapi.com/v1/current.json?key=f8cef05d076a409f853153615233008&q=Żary&aqi=no');
+    print(response.data);
     return const WeatherModel(city: 'Warsaw', temperature: -5.5);
   }
 }
